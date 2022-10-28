4b4d33cbcdf008c5350d8314d8142c791394f330
xuanweiace <xuanweiace@163.com>
2022-10-29 01:43:36 +0800

    1、完成http部分逻辑（注册、登录、登出） 2、完成序列化与反序列化的工作（增加对Enum解析的支持） 3、增加对cli_session的维护（未测试）

--- src/NGGService.py
+++ src/NGGService.py
@@ -1,7 +1,8 @@
 from repository.RoomPO import RoomPO
 from repository.crud import roomCRUD, playerCRUD
 from Generator import RoomGenerator
-
+from BaseObject import Room
+import constant
 
 
 class NGGService:
@@ -9,7 +10,7 @@ class NGGService:
         
         pass
     
-    def createRoom(userId:int)->int:
+    def createRoom(data: dict)->int:
         """
         用户userId想要创建房间，我们只需要分配一个房间号roomId返回即可，同时把roomid和userid绑定关系存一下。
         这是向外提供的接口，所以：
@@ -17,16 +18,21 @@ class NGGService:
             2、只需要userID就行了，不需要完整player信息
         """
         # todo 需要解决并发问题
-        room = RoomGenerator.generateRoom()
-        roomid = roomCRUD.insert(RoomPO.bo2po())
+        userId:int = data["userid"]
+        roomname:str = data["roomname"]
+        
+        room = Room(-1, roomname, constant.ObjectType.NORMAL_ROOM, [], [])
+        roomid = roomCRUD.insert_without_id(RoomPO.bo2po())
         return roomid
         
     def createGame():
         # todo
         pass
         
-        
-        
+    
+    def getHandler(self, api: str):
+        if api == '/create_room':
+            return self.createRoom
         
         
 nggService = NGGService()   
--- src/main.py
+++ src/main.py
@@ -2,71 +2,91 @@ import asyncio
 import add_path
 import json
 import websockets
+import traceback
 from websockets.legacy.protocol import WebSocketCommonProtocol
 from loguru import logger
-from NGGContext import ngg_session, NGGContext
+from NGGContext import can_welcome_client, new_client_welcome, deal_client_leave
 from NGGService import nggService
 from AppService import appService
+from CODE import pack_err_dict
+import err
+
+"""
+注意route_ngg和route_http的异常捕获逻辑不能抽取到一起，
+因为异常处理逻辑不一样，route_ngg是有状态的，需要释放连接
+"""
 
 # 该函数维护了一个websocket长连接
 
 async def route_ngg(conn: WebSocketCommonProtocol):
-    pass
+    logger.debug("conn:{}".format(conn))
+    # 如果已有ip再次过来，就拒绝连接。
+    cli_host, cli_port = conn.remote_address
+    # if can_welcome_client(cli_host, cli_port) == False:
+    #     await conn.send(f"{'code':{err.ER.CLIENT_ALREADY_CONNECTED},'msg':'该ip已被占用,ip={cli_host}'}")
+    #     return
+    
+    try: 
+        # new_client_welcome(conn, cli_host, cli_port)
+        async for message in conn:
+            logger.info("got a message:{}".format(message))
+            data_d = json.loads(message)
+            handler = appService.getHandler(data_d['api'])
+            if path == '/create_room':
+                newRoomId = nggService.createRoom(103)
+                await conn.send("create room success")
+            if handler != None:
+                res = handler(data_d["data"])
+            else:
+                res = pack_err_dict(err.ER.NO_REQUEST_HANDLER, "服务端没有该api请求的处理函数")
+            send_msg = json.dumps(res)
+            await conn.send(send_msg)                
+    except Exception as e:
+        erro = traceback.format_exc()
+        logger.error(f"[main.route_ngg] remote_address{conn.remote_address} errpr={erro}")
+    finally:
+        # 释放连接， 但是不需要关闭conn（退出上面for的时候就退出了）
+        deal_client_leave(cli_host, cli_port)
 
 
+# 只进行一次通信的短连接，类似http，无状态
 async def route_http(conn: WebSocketCommonProtocol):
     try:
         async for message in conn:
             data_d = json.loads(message)
-            
+            logger.info(f"[main.route_http] request:{data_d}")
             handler = appService.getHandler(data_d['api'])
-            res = {'api':data_d['api'], 'code': 40002, "msg": "服务端没有该请求的处理函数", "data": None}
-            print("res:", res)
-            print("json.dumps(res)",json.dumps(res))
+            res = pack_err_dict(err.ER.NO_REQUEST_HANDLER, "服务端没有该api请求的处理函数")
             if handler != None:
-                res = handler(data_d)
-            
-            return res
+                res = handler(data_d["data"])
+            else:
+                res = pack_err_dict(err.ER.NO_REQUEST_HANDLER, "服务端没有该api请求的处理函数")
+            send_msg = json.dumps(res)
+            await conn.send(send_msg)
+            return
         
     except Exception as e:
-        logger.error(f"[main.route_http] remote_address{conn.remote_address} err={e}")
+        send_msg = json.dumps(pack_err_dict(err.ER.SERVER_ERROR, str(e)))
+        await conn.send(send_msg)
+        erro = traceback.format_exc()
+        logger.error(f"[main.route_http] remote_address{conn.remote_address} error={erro}")
     finally:
         #todo
         pass
                 
 
-async def echo(conn: WebSocketCommonProtocol, path):
+async def echo(conn: WebSocketCommonProtocol, path):    
     if path == '/ngg': # 需要维护状态
-        route_ngg(conn)
+        await route_ngg(conn)
     elif path == '/http': # 无状态协议
-        route_http(conn)
+        await route_http(conn)
     else:
+        logger.warning(f"[main.echo] ws路径不支持: path={path}")
+        await conn.send("error")
+        await conn.close()
         
         
-    logger.debug("conn:{}".format(conn))
-    logger.debug("path:{}".format(path))
-    print("remote_address:", conn.remote_address)
-    print("path:", path)
-    # 如果已有ip再次过来，就拒绝连接。
-    cli_host, cli_port = conn.remote_address
-    if cli_host in ngg_session:
-        await conn.send(f"{'code':40001,msg:'该ip已被占用{cli_host}'}")
-        return
-    ngg_session[cli_host] = NGGContext()
-    
-    try: 
-        async for message in conn:
-            logger.info("got a message:{}".format(message))
-            if path == '/create_room':
-                print(f"进来了，path={path}")
-                newRoomId = nggService.createRoom(103)
-                await conn.send("create room success")
-                
-    except Exception as e:
-        logger.error(f"[main.route_ngg] remote_address{conn.remote_address} err={e}")
-    finally:
-    # 释放连接
-        ngg_session.pop(cli_host)
+
 
 async def main():
     # start a websocket server
